80ab28fef27cce3284d295f59b01169e5abc738b
Rivindu Wanniachchi <rivindu.15@cse.mrt.ac.lk>
2019-08-05 22:26:15 +0530

    Added Start Screen and functionality, added audio, enabled tilting in android devices for car movement

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -7,27 +7,108 @@ public class carController : MonoBehaviour
     public float carSpeed;
     public float maxPos = 2.2f;
 
+    public uiManager ui;
+    public audioManager audio;
+    bool currentPlatformAndroid = false;
 
     Vector3 position;
+    Rigidbody2D rigidbody2D;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        #if UNITY_ANDROID
+
+                currentPlatformAndroid = true;
+        #else
+
+                currentPLatformAndroid = false;
+        #endif
+
+        audio.carSound.Play();
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        
         position = transform.position;
+
+        if (currentPlatformAndroid == true)
+        {
+            Debug.Log("Android");
+        }
+
+        else
+        {
+            Debug.Log("Not Android");
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        if (currentPlatformAndroid == true)
+        {
+            AcceleromemterMove();
+        }
+        else
+        {
+            position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+            position.x = Mathf.Clamp(position.x, -2.2f, 2.2f);
+            transform.position = position;
+        }
+        position = transform.position; 
         position.x = Mathf.Clamp(position.x, -2.2f, 2.2f);
         transform.position = position;
+
     }
+
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Enemy Car")
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+            ui.GameOverActivated();
+            audio.carSound.Stop();
+        }
+    }
+
+    public void MoveRight()
+    {
+        rigidbody2D.velocity = new Vector2(carSpeed, 0);
+    }
+
+    public void MoveLeft()
+    {
+        rigidbody2D.velocity = new Vector2(-carSpeed, 0);
+    }
+
+    public void SetVelocityToZero()
+    {
+        rigidbody2D.velocity = Vector2.zero;
+    }
+    void AcceleromemterMove()
+    {
+        float x = Input.acceleration.x;
+        Debug.Log("X =" + x);
+        if (x < -0.1f)
+        {
+            MoveLeft();
+        }
+        else if (x > 0.1f)
+        {
+            MoveRight();
+        }
+        else
+        {
+            SetVelocityToZero();
         }
     }
 }
--- Assets/Scripts/uiManager.cs
+++ Assets/Scripts/uiManager.cs
@@ -1,21 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class uiManager : MonoBehaviour
 {
+    public Button [] buttons;
+    public Text scoreText;
+    bool gameOver;
+    int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver = false;
+        score = 0;
+        InvokeRepeating("ScoreUpdate", 1.0f,0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score :" + score;
+        print(score);
     }
 
+    void ScoreUpdate()
+    {   if(gameOver == false)
+        {
+            score += 1;
+        }
+       
+    }
+
+    public void GameOverActivated()
+    {
+        gameOver = true;
+        foreach(Button button in buttons) {
+            button.gameObject.SetActive(true);
+        }
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("level1");
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("menuScene");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
     public void Pause()
     {
         if(Time.timeScale == 1)
